fb795e47d8885f1a7f5d89a3c128b45f2e8c0a74
Thibaud Allain <thibaudallainta@gmail.com>
2020-05-09 18:36:27 +0200

    mapbox ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @bike_parks = BikePark.geocoded
+
+    @markers = @bike_parks.map do |bike_park|
+      {
+        lat: bike_park.lat,
+        lng: bike_park.lng
+      }
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,9 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { initMapbox } from '../plugins/init_mapbox';
 import "bootstrap";
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})

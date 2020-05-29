68c25cf26645eec1b3e1006ad56657eaad700588
Thibaud Allain <thibaudallainta@gmail.com>
2020-05-29 19:55:55 +0200

    changed style of algolia results

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,7 +3,7 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('search_address');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput, accessibility: { clearButton: { 'tab-index': 13,} } });
   }
 };
 

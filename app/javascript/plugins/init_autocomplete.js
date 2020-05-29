5ffc60521c07e3e12c6f5d8ac6491334e9dd3e00
Thibaud Allain <thibaudallainta@gmail.com>
2020-05-29 20:11:04 +0200

    restricted algolia to france

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,7 +3,7 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('search_address');
   if (addressInput) {
-    places({ container: addressInput, accessibility: { clearButton: { 'tab-index': 13,} } });
+    places({ container: addressInput, accessibility: { clearButton: { 'tab-index': 13,} }, countries: ['FR'], });
   }
 };
 
